c2c2c3da051aa80ed7654d2efda57900527a8a73
anton.pawlik <anton.pawlik@a22c2dd8-79d1-11de-96f2-af6933d8d261>
2009-09-03 19:38:46 +0000

    i think thats it 0.9.4 is ready for some real world tests

--- index.php
+++ index.php
@@ -7,29 +7,14 @@
  * @copyright  (c) 2009 Anton Pawlik
  * @license    http://xcss.antpaw.org/about/
  */
-$time_start = microtime_float();
+
 
 define('XCSSCLASS', 'xcss-class.php');
-file_exists(XCSSCLASS) ? include XCSSCLASS : die('alert("Can\'t find the xCSS class file: \''.XCSSCLASS.'\'"!);');
+file_exists(XCSSCLASS) ? include XCSSCLASS : die('alert("xCSS Parse error: Can\'t find the xCSS class file: \''.XCSSCLASS.'\'"!);');
 
 define('XCSSCONFIG', '_config.php');
-file_exists(XCSSCONFIG) ? include XCSSCONFIG : die('alert("Can\'t find the xCSS config file: \''.XCSSCONFIG.'\'"!);');
+file_exists(XCSSCONFIG) ? include XCSSCONFIG : die('alert("xCSS Parse error: Can\'t find the xCSS config file: \''.XCSSCONFIG.'\'"!);');
 
 $xCSS = new xCSS($config);
 
 $xCSS->compile();
-
-
-
-
-
-
-usleep(30000);
-$time = microtime_float() - $time_start;
-echo round($time, 6)." seconds\n";
-
-function microtime_float()
-{
-    list($usec, $sec) = explode(" ", microtime());
-    return ((float)$usec + (float)$sec);
-}
