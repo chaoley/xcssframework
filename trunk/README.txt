94987bd148c5e0db7b8aa191a1b76de06c3f1978
anton.pawlik <anton.pawlik@a22c2dd8-79d1-11de-96f2-af6933d8d261>
2009-10-14 16:25:16 +0000

    0.9.5 final

--- trunk/README.txt
+++ trunk/README.txt
@@ -1,5 +1,5 @@
 Name:		xCSS
-Version:	v0.9.4
+Version:	v0.9.5
 License:	see LICENSE
 Author:		Anton Pawlik
 Year:		2009
--- trunk/index.php
+++ trunk/index.php
@@ -12,7 +12,7 @@
 define('XCSSCLASS', 'xcss-class.php');
 file_exists(XCSSCLASS) ? include XCSSCLASS : die('alert("xCSS Parse error: Can\'t find the xCSS class file: \''.XCSSCLASS.'\'.");');
 
-define('XCSSCONFIG', '_config.php');
+define('XCSSCONFIG', 'config.php');
 file_exists(XCSSCONFIG) ? include XCSSCONFIG : die('alert("xCSS Parse error: Can\'t find the xCSS config file: \''.XCSSCONFIG.'\'.");');
 
 $xCSS = new xCSS($config);
