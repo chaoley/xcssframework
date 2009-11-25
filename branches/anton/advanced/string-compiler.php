6e0225471af61a6cddc432203bc9a05a958fb566
anton.pawlik <anton.pawlik@a22c2dd8-79d1-11de-96f2-af6933d8d261>
2009-11-25 21:03:52 +0000

    0.9.8

--- branches/anton/advanced/string-compiler.php
+++ branches/anton/advanced/string-compiler.php
@@ -8,9 +8,16 @@ $xcss_string = '.selector { color: red; }';
 
 define('XCSSCLASS', '../xcss-class.php');
 include XCSSCLASS;
-define('XCSSCONFIG', '../config.php');
+define('XCSSCONFIG', '../_config.php');
 include XCSSCONFIG;
 
 $xCSS = new xCSS($config);
 
-echo $xCSS->compile($xcss_string);
+echo '<script type="text/javascript">'."\n";
+$css_string = $xCSS->compile($xcss_string);
+unset($xCSS);
+echo '</script>'."\n";
+
+echo '<style type="text/css">'."\n";
+echo $css_string;
+echo '</style>'."\n";
